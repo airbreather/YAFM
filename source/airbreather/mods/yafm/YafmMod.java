06bee16d4c2ee7072bbe2131a09cdd7cc8fee895
Joe Amenta <airbreather@linux.com>
2014-01-11 11:20:15 -0500

    Updating to the latest version of airbreathercore

--- source/airbreather/mods/yafm/YafmMod.java
+++ source/airbreather/mods/yafm/YafmMod.java
@@ -1,28 +1,51 @@
 package airbreather.mods.yafm;
 
 import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLInitializationEvent;
 import cpw.mods.fml.common.event.FMLPostInitializationEvent;
 import cpw.mods.fml.common.event.FMLPreInitializationEvent;
 
-import airbreather.mods.airbreathercore.mod.ModBase;
+import airbreather.mods.airbreathercore.mod.IModLifecycleManager;
+import airbreather.mods.airbreathercore.mod.IModule;
+import airbreather.mods.airbreathercore.mod.ModLifecycleManager;
+
+import static com.google.common.base.Preconditions.checkNotNull;
 
 @Mod(modid = YafmConstants.ModID, name = YafmConstants.ModID, version = YafmConstants.CurrentVersion)
-public final class YafmMod extends ModBase
+public final class YafmMod
 {
-    public YafmMod()
+    private final IModLifecycleManager modLifecycleManager;
+
+    public YafmMod(final IModLifecycleManager modLifecycleManager)
     {
-        super(new YafmModule());
+        this.modLifecycleManager = checkNotNull(modLifecycleManager, "modLifecycleManager");
+    }
+
+    // Either a parameterless constructor or a parameterless static factory
+    // method is required for FML to load us.
+    @Mod.InstanceFactory
+    private static YafmMod CreateInstance()
+    {
+        IModule module = new YafmModule();
+        IModLifecycleManager modLifecycleManager = new ModLifecycleManager(module);
+        return new YafmMod(modLifecycleManager);
     }
 
     @Mod.EventHandler
-    public void preInit(FMLPreInitializationEvent event)
+    private void PreInit(FMLPreInitializationEvent event)
     {
-        this.preInitCore(event);
+        this.modLifecycleManager.OnPreInit(event);
     }
 
     @Mod.EventHandler
-    public void postInit(FMLPostInitializationEvent event)
+    private void Init(FMLInitializationEvent event)
     {
-        this.postInitCore(event);
+        this.modLifecycleManager.OnInit(event);
+    }
+
+    @Mod.EventHandler
+    private void PostInit(FMLPostInitializationEvent event)
+    {
+        this.modLifecycleManager.OnPostInit(event);
     }
 }
